cddcc7e5735022c7a4faeee5331e7b80a6349406
takumak <deu48803@kwansei.ac.jp>
2017-07-17 02:16:09 +0900

    Fix (parse|generate) table header label function

--- src/functions.py
+++ src/functions.py
@@ -1,17 +1,19 @@
 def getTableColumnLabel(c):
   label = ''
   while True:
-    label += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'[c % 26]
-    if c <= 26:
+    label = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'[c % 26] + label
+    if c < 26:
       break
-    c = int(c/26)
+    c = c//26-1
   return label
 
 def parseTableColumnLabel(label):
-  ret = 0
-  for c in map(ord, reversed(label)):
+  if not label:
+    raise ValueError('Invalid label: %s' % label)
+  ret = -1
+  for c in map(ord, label):
     if 0x41 <= c <= 0x5A:
-      ret = ret*26 + (c-0x41)
+      ret = (ret+1)*26 + (c-0x41)
     else:
       raise ValueError('Invalid label: %s' % label)
   return ret
